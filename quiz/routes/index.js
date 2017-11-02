486396812dbe7e5356ae1868340ae73e0aee5a19
smbrown1 <StevenMichaelBrown1@gmail.com>
2017-11-02 20:14:44 +0000

    added getUser rout

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+var trivia = "https://opentdb.com/api.php?amount=10&type=boolean"; 
+router.get('/getQuestions', function(req, res){
+	request(trivia).pipe(res);
+});
+
+var users = [];
+router.get('/getUser', function(req, res){
+	for(var i=0; i<users.length; i++)
+	{
+		if(users[i][0] == req.query.q)
+		{
+			ret.status(200).json({username:users[i][0]});
+			return;
+		}
+	}
+	users.push([req.query.q, 0]);
+	ret.status(200).json({username:users[i][0]});
 });
 
 module.exports = router;
